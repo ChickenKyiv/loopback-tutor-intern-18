178336cf36e346c7ff872b7daf5f098676f5a827
jrp023es <jorge.rodriguezperez@diagroup.com>
2018-05-21 16:48:48 +0200

    Creates validations for campagrounds and reservations

--- server/models/campground.js
+++ server/models/campground.js
@@ -1,5 +1,8 @@
-'use strict';
+"use strict";
 
 module.exports = function(Campground) {
-
+  Campground.validatesLengthOf("name", {
+    max: 100,
+    message: { max: "Name is too long" }
+  });
 };
--- server/models/reservation.js
+++ server/models/reservation.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
 module.exports = function(Reservation) {
+  function dateValidator(err) {
+    if (this.startDate >= this.endDate) {
+      err();
+    }
+  }
 
+  Reservation.validate("startDate", dateValidator, {
+    message: "endDate should be after startDate"
+  });
 };
