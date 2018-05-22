5559984636a7460d944e34ca470c321c04607b4f
jrp023es <jorge.rodriguezperez@diagroup.com>
2018-05-22 13:08:59 +0200

    set access_token as default header for token

--- server/server.js
+++ server/server.js
@@ -1,22 +1,31 @@
-'use strict';
+"use strict";
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+var loopback = require("loopback");
+var boot = require("loopback-boot");
 
-var app = module.exports = loopback();
+var app = (module.exports = loopback());
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    app.emit("started");
+    var baseUrl = app.get("url").replace(/\/$/, "");
+    console.log("Web server listening at: %s", baseUrl);
+    if (app.get("loopback-component-explorer")) {
+      var explorerPath = app.get("loopback-component-explorer").mountPath;
+      console.log("Browse your REST API at %s%s", baseUrl, explorerPath);
     }
   });
 };
+
+app.use(
+  loopback.token({
+    cookies: ["access_token"],
+    headers: ["access_token", "X-Access-Token"],
+    params: ["access_token"]
+    //additional keys (check docs for more info)
+  })
+);
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -24,6 +33,5 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
