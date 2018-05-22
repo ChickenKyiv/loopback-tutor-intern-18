6a4af5129a5a2cc5f1a2d4f1e7d24ed85b9cdb21
Jorge Rodriguez Perez <jorgerodriguezperez833@gmail.com>
2018-05-22 22:00:41 +0200

    Creates service for sending email

--- server/models/reservation.js
+++ server/models/reservation.js
@@ -10,4 +10,22 @@ module.exports = function(Reservation) {
   Reservation.validate("startDate", dateValidator, {
     message: "endDate should be after startDate"
   });
+
+  Reservation.observe("after save", function(ctx, next) {
+    Reservation.app.models.Campground.findById(ctx.instance.campgroundId, function(err, campground) {
+      Reservation.app.models.Email.send({
+        to: 'loopbackintern@yopmail.com',
+        from: 'noreply@optis.be',
+        subject: 'Thank you for your reservation at ' + campground.name,
+        html: '<p>We confirm your reservation for <strong>' + campground.name + '</strong></p>'
+      }, function (err, mail) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log('email sent!');
+        }
+      });
+    });
+    next();
+  })
 };
