ccbe7ed4ccb33c31e34be687cf3d505a596b4d2f
jrp023es <jorge.rodriguezperez@diagroup.com>
2018-05-28 08:03:58 +0200

    Next callback error

--- server/models/reservation.js
+++ server/models/reservation.js
@@ -1,6 +1,8 @@
 "use strict";
 const Raven = require("raven");
-Raven.config('https://77aa2ee9a7ce484497f56278982a0809@sentry.io/305339').install()
+Raven.config(
+  "https://77aa2ee9a7ce484497f56278982a0809@sentry.io/305339"
+).install();
 
 module.exports = function(Reservation) {
   function dateValidator(err) {
@@ -10,37 +12,50 @@ module.exports = function(Reservation) {
   }
 
   function sendEmail(campground) {
-     Reservation.app.models.Email.send(formEmailObject(campground),function(err, mail) {
-      if(err) {
-        console.log(err)
-        Raven.captureException(err);
-      } else {
-        console.log(mail);
-        console.log('email sent!');
-      }
-    })
+    new Promise((resolve, reject) => {
+      Reservation.app.models.Email.send(formEmailObject(campground), function(
+        err,
+        mail
+      ) {
+        if (err) {
+          console.log(err);
+          Raven.captureException(err);
+          reject(err);
+        } else {
+          console.log(mail);
+          console.log("email sent!");
+          resolve(mail);
+        }
+      });
+    });
   }
 
   function formEmailObject(campground) {
     return {
-      to: 'loopbackintern@yopmail.com',
-      from: 'noreply@optis.be',
-      subject: 'Thank you for your reservation at ' + campground.name,
-      html: '<p>We confirm your reservation for <strong>' + campground.name + '</strong></p>'
-    }
+      to: "loopbackintern@yopmail.com",
+      from: "noreply@optis.be",
+      subject: "Thank you for your reservation at " + campground.name,
+      html:
+        "<p>We confirm your reservation for <strong>" +
+        campground.name +
+        "</strong></p>"
+    };
   }
 
   Reservation.validate("startDate", dateValidator, {
     message: "endDate should be after startDate"
   });
 
-   Reservation.observe("after save", async function(ctx, next) {
-     try {
-       const campground = await Reservation.app.models.Campground.findById(ctx.instance.campgroundId);
-       sendEmail(campground);
-     } catch(e) {
-       Raven.captureException(e);
-       next(e);
-     }
-   })
+  Reservation.observe("after save", async function(ctx, next) {
+    try {
+      const campground = await Reservation.app.models.Campground.findById(
+        ctx.instance.campgroundId
+      );
+      const mail = await sendEmail(campground);
+      next();
+    } catch (e) {
+      Raven.captureException(e);
+      next(e);
+    }
+  });
 };
