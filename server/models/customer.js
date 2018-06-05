3fc3b51c6f61c5a21fc95f8d89baef3c0ea2bf14
Jorge Rodriguez Perez <jorgerodriguezperez833@gmail.com>
2018-06-05 19:55:08 +0200

    Creates role after user

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(Customer) {
+const server = require('../server');
 
+module.exports = function(Customer) {
+    Customer.observe("after save", (ctx, next) => {
+        const Role = server.models.Role;
+        const RoleMapping = server.models.RoleMapping;
+        console.log(ctx);
+        Role.findOne({where: {name: 'admin'}}, function(err, adminRole) {
+            if(err) {
+                Role.create({name: 'admin'}).then((role) => {
+                    role.principals.create({
+                        principalType: RoleMapping.USER,
+                        principalId: ctx.instance.id
+                    }, function(err, principal) {
+                        console.log('Principal ', principal);
+                        next();
+                    })
+                }).catch(err => {
+                    throw err;
+                })
+            } else {
+                adminRole.principals.create({
+                    principalType: RoleMapping.CUSTOMER,
+                    principalId: ctx.instance.id
+                }, function(err, principal) {
+                    console.log('Principal ', principal);
+                    next();
+                })  
+            }
+        })
+    })
 };
