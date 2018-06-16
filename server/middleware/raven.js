c7fcb6e2dbecf290ffe249ccc97689209ec69730
Jorge Rodriguez Perez <jorgerodriguezperez833@gmail.com>
2018-06-16 17:46:22 +0200

    #14 Adds raven uri constant

--- server/middleware/raven.js
+++ server/middleware/raven.js
@@ -1,7 +1,6 @@
 const Raven = require("raven");
-Raven.config(
-  "https://77aa2ee9a7ce484497f56278982a0809@sentry.io/305339"
-).install();
+const CONSTANTS = require("../constants");
+Raven.config(CONSTANTS.RAVEN_URI).install();
 
  module.exports = function () {
      return function raven(err, req, res, next) {
